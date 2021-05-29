f92aed684f5db068cbdc164256b75118ff76fa96
Florin Munteanu <florinmunteanu96@gmail.com>
2021-05-29 10:30:35 +0300

    Extract method

--- middleware/withUser.ts
+++ middleware/withUser.ts
@@ -1,26 +1,31 @@
-import { handleCallback, getSession } from "@auth0/nextjs-auth0";
-import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
-import { User } from "../models/User";
-import DynamoDAO from "../services/DynamoDAO";
+import { getSession } from "@auth0/nextjs-auth0";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { ensureAuth0Exists } from "../services/UserService";
 
 declare module "next" {
     export interface NextApiRequest {
-        user: import("../models/User").User
+        user: import("../models/User").User | undefined
     }
+}
+
+export const getUserFromRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+    const auth0User = getSession(req, res)?.user;
+
+    if (auth0User) {
+        const user = await ensureAuth0Exists(auth0User);
+        return user;
+    }
+
+    return undefined;
 }
 
 const withUser = (handler: NextApiHandler) => {
     return async (req: NextApiRequest, res: NextApiResponse) => {        
-        const auth0User = getSession(req, res)?.user;
+        const user = await getUserFromRequest(req, res);
 
-        if(auth0User) {
-            const user = await ensureAuth0Exists(auth0User);              
-            req.user = user;
-        }
-
+        req.user = user;
         return handler(req, res);
-    };
-};
+    };    
+};    
 
 export default withUser;
