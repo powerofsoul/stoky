e5f383332a6e30a33e652e65f62c13b70dd0fd93
Florin Munteanu <florinmunteanu96@gmail.com>
2021-05-30 00:10:40 +0300

    Fix wrong to date

--- pages/api/stock/historical.ts
+++ pages/api/stock/historical.ts
@@ -1,14 +1,14 @@
 import { CallbackOptions } from "@auth0/nextjs-auth0";
+import moment from "moment";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const yahooFinance = require("yahoo-finance");
 
 export default async (req: NextApiRequest, res: NextApiResponse<any>, options?: CallbackOptions | undefined) => {
-    const date = new Date();
     const historical = await yahooFinance.historical({
         symbol: req.query.symbol,
-        from: '2012-01-01',
-        to: `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`,
+        from: '2018-01-01',
+        to: moment().format("YYYY-MM-DD"),
         period: 'd'
       });
 
