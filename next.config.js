671d19ae0520034bdc1d1e5c07571c2e505dc44a
Baba, Mihai <mihaibaba34@gmail.com>
2021-06-01 22:57:15 +0300

    Added a prefix for assets in production env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 // next.config.js
 const withTM = require("next-transpile-modules")(["request"]);
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withTM();
+module.exports = withTM({
+  assetPrefix: isProd ? '/prod' : ''
+});
