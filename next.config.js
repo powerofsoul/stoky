a136bc4e4644e01aeddd9e6b6c3a91f43b72988a
Baba, Mihai <mihaibaba34@gmail.com>
2021-06-02 09:29:35 +0300

    Revert "Added a prefix for assets in production env"
    This reverts commit 671d19ae0520034bdc1d1e5c07571c2e505dc44a.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 // next.config.js
 const withTM = require("next-transpile-modules")(["request"]);
-const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withTM({
-  assetPrefix: isProd ? '/prod' : ''
-});
+module.exports = withTM();
