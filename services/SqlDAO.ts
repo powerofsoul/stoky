b1045c2639ceb9c64e592ed953e41c4920d162f0
Florin Munteanu <florinmunteanu96@gmail.com>
2021-06-06 01:06:48 +0300

    Fix local env creating multiple connections on hot reload

--- services/SqlDAO.ts
+++ services/SqlDAO.ts
@@ -1,5 +1,21 @@
-import { PrismaClient } from '@prisma/client'
+/* eslint import/no-mutable-exports: "off" */
 
-const SqlDAO = new PrismaClient()
+import { Prisma, PrismaClient } from '@prisma/client'
+
+// On local everything is reinitialized due to hot reloading. This means we create unnecessary connections to db
+let SqlDAO: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+>
+
+if (process.env.NODE_ENV === 'production') {
+    SqlDAO = new PrismaClient()
+} else {
+    if (!global.SqlDAO) {
+        global.SqlDAO = new PrismaClient()
+    }
+    SqlDAO = global.SqlDAO
+}
 
 export default SqlDAO
