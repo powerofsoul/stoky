1b5d87125a2b1fcf84160f4efbb24ec2c4cbefb8
Florin Munteanu <florinmunteanu96@gmail.com>
2021-06-06 19:19:54 +0300

    Fix viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,19 @@ import 'react-toastify/scss/main.scss'
 import '../styles/global.scss'
 import { ToastContainer } from 'react-toastify'
 import { AppWrapper } from '../src/context/AppContext'
+import React from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <UserProvider>
             <AppWrapper>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+                    />
+                </Head>
                 <ToastContainer />
                 <Component {...pageProps} />
             </AppWrapper>
