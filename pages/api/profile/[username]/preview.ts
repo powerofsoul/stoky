0e86d87973bb33df36de0bdf0942b2da994a8e67
Florin Munteanu <florinmunteanu96@gmail.com>
2021-06-11 23:49:22 +0300

    Remove middleware for profile preview

--- pages/api/profile/[username]/preview.ts
+++ pages/api/profile/[username]/preview.ts
@@ -7,58 +7,56 @@ import { randomColor } from '../../../../src/Utils'
 
 const { ChartJSNodeCanvas } = require('chartjs-node-canvas')
 
-export default withUser(
-    async (req: NextApiRequest, res: NextApiResponse<any>, options?: CallbackOptions | undefined) => {
-        const { username } = req.query as { [key: string]: string }
+export default async (req: NextApiRequest, res: NextApiResponse<any>, options?: CallbackOptions | undefined) => {
+    const { username } = req.query as { [key: string]: string }
 
-        const user = await SqlDAO.user.findFirst({
-            where: {
-                username,
-            },
-        })
+    const user = await SqlDAO.user.findFirst({
+        where: {
+            username,
+        },
+    })
 
-        if (!user) {
-            res.redirect('/')
-            return
-        }
+    if (!user) {
+        res.redirect('/')
+        return
+    }
 
-        const chartJSNodeCanvas = new ChartJSNodeCanvas({
-            width: 1200,
-            height: 617,
-        })
+    const chartJSNodeCanvas = new ChartJSNodeCanvas({
+        width: 1200,
+        height: 617,
+    })
 
-        const portfolioEvents = await SqlDAO.portfolioTicker.findMany({
-            where: {
-                userId: user.id,
-            },
-        })
+    const portfolioEvents = await SqlDAO.portfolioTicker.findMany({
+        where: {
+            userId: user.id,
+        },
+    })
 
-        const configuration = {
-            type: 'doughnut',
-            data: {
-                labels: portfolioEvents.map((p) => p.symbol),
-                datasets: [
-                    {
-                        label: `test`,
-                        data: portfolioEvents.map((p) => p.amount * p.averagePrice),
-                        backgroundColor: portfolioEvents.map(() => randomColor()),
-                    },
-                ],
-            },
-            options: {
-                plugins: {
-                    title: {
-                        display: true,
-                        text: `${user.username} portfolio`,
-                    },
+    const configuration = {
+        type: 'doughnut',
+        data: {
+            labels: portfolioEvents.map((p) => p.symbol),
+            datasets: [
+                {
+                    label: `test`,
+                    data: portfolioEvents.map((p) => p.amount * p.averagePrice),
+                    backgroundColor: portfolioEvents.map(() => randomColor()),
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: `${user.username} portfolio`,
                 },
             },
-        }
-
-        const image = await chartJSNodeCanvas.renderToBuffer(configuration)
-        res.setHeader('Content-Type', 'image/png')
-        res.setHeader('Accept', 'image/png')
-        res.status(200).write(image)
-        res.end()
+        },
     }
-)
+
+    const image = await chartJSNodeCanvas.renderToBuffer(configuration)
+    res.setHeader('Content-Type', 'image/png')
+    res.setHeader('Accept', 'image/png')
+    res.status(200).write(image)
+    res.end()
+}
