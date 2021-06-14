9297781d3ae07ac3b77e31e0d1aff4b5780cb5a9
Florin Munteanu <florinmunteanu96@gmail.com>
2021-06-14 22:39:25 +0300

    Fix gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,27 +3,30 @@ import 'react-toastify/scss/main.scss'
 import 'tippy.js/dist/tippy.css' // optional
 import '../styles/global.scss'
 import { ToastContainer } from 'react-toastify'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/dist/client/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url: URL) => {
+            // @ts-ignore
+            gtag.pageview(url)
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events])
+
     return (
         <>
             <Head>
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-                />
-
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', 'G-N7XCNVZ9M7', { page_path: window.location.pathname });
-                                `,
-                    }}
                 />
             </Head>
             <ToastContainer />
